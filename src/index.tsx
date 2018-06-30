91aa68472ce0c1245ff2bfb50f270f3dfde344fa
Frederic Godin <godin.fred8@gmail.com>
2018-06-30 15:49:22 +0200

    Handle enter animations with React portal

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,66 @@
-import { createElement, Component, ReactNode } from 'react';
+import { Component, ReactNode, ReactPortal } from 'react';
+import { createPortal } from 'react-dom';
+import * as classnames from 'classnames';
+
+const TICK_TIMEOUT = 50;
 
 export interface Props {
   children: ReactNode;
+  className: string;
+  classNames: {
+    enter: string;
+    enterActive: string;
+    leave: string;
+    leaveActive: string;
+  };
+  timeout: number;
 }
 
-export interface State {}
+export interface State {
+  children: ReactNode;
+}
 
 class AniPortal extends Component<Props, State> {
-  static defaultProps = {};
+  container: HTMLDivElement = document.createElement('div');
 
   constructor(props: Props) {
     super(props);
+    this.state = {
+      children: props.children,
+    };
   }
 
-  render() {
-    return <div>{this.props.children}</div>;
+  static getDerivedStateFromProps(props: Props): State {
+    console.log('GET DERIVED STATE');
+    return {
+      children: props.children,
+    };
+  }
+
+  componentDidMount() {
+    const { className, classNames, timeout } = this.props;
+    const enterClassName = classnames(className, classNames.enter);
+    const enterActiveClassName = classnames(className, classNames.enter, classNames.enterActive);
+
+    this.container.className = enterClassName;
+    document.body.appendChild(this.container);
+
+    setTimeout(() => (this.container.className = enterActiveClassName), TICK_TIMEOUT);
+    setTimeout(() => (this.container.className = enterActiveClassName), timeout + TICK_TIMEOUT);
+  }
+
+  componentWillUnmount() {
+    const { className, classNames, timeout } = this.props;
+    const leaveClassName = classnames(className, classNames.leave);
+    const leaveActiveClassName = classnames(className, classNames.leave, classNames.leaveActive);
+
+    this.container.className = leaveClassName;
+    setTimeout(() => (this.container.className = leaveActiveClassName), TICK_TIMEOUT);
+    setTimeout(() => document.body.removeChild(this.container), timeout + TICK_TIMEOUT);
+  }
+
+  render(): ReactPortal {
+    return createPortal(this.state.children, this.container);
   }
 }
 
--- src/stories.tsx
+++ src/stories.tsx
@@ -1,5 +1,44 @@
-import { createElement } from 'react';
+import { createElement, Fragment, Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import AniPortal from './index';
 
-storiesOf('MultiAutoComplete', module).add('default', () => <AniPortal>Hello Portal</AniPortal>);
+// classNames are defined in `.storybook/preview-head.html`
+const portalClassName = 'portal-example';
+const portalClassNames = {
+  enter: 'portal-example_enter',
+  enterActive: 'portal-example_enter-active',
+  leave: 'portal-example_leave',
+  leaveActive: 'portal-example_leave-active',
+};
+
+storiesOf('MultiAutoComplete', module).add('default', () => <Example1 />);
+
+class Example1 extends Component<{}, { opened: boolean }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { opened: false };
+    this.openPortal = this.openPortal.bind(this);
+    this.closePortal = this.closePortal.bind(this);
+  }
+  openPortal() {
+    this.setState({ opened: true });
+  }
+  closePortal() {
+    this.setState({ opened: false });
+  }
+  render() {
+    return (
+      <Fragment>
+        <button onClick={this.openPortal}>Open portal</button>
+        {this.state.opened && (
+          <AniPortal className={portalClassName} classNames={portalClassNames} timeout={1000}>
+            <Fragment>
+              Hello Portal
+              <button onClick={this.closePortal}>Close Portal</button>
+            </Fragment>
+          </AniPortal>
+        )}
+      </Fragment>
+    );
+  }
+}
