cbeef14a0e2d13f1d4c62ebe91aaa433266b8d5d
Epimodev <godin.fred8@gmail.com>
2019-06-14 19:52:42 +0200

    Upgrade dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,3 @@
-
 import { configure } from '@storybook/react';
 
 const req = require.context('../src', true, /stories\.tsx$/)
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path')
 
 const projectRoot = path.resolve(__dirname, '../');
 
-/**
- * We use ts-loader instead of awesome-typescript-loader
- * becase storybook is not compatible with last version of ts-loader
- */
-
 module.exports = {
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
